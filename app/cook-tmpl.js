0994f3c660cfe3744ebf41632ed7255f28b3b5c4
Igor Schelkonogov <ingver.yum@gmail.com>
2016-10-30 22:10:37 +0500

    Fixed non-existing dir fail

--- app/cook-tmpl.js
+++ app/cook-tmpl.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 const fileName = 'todo-list';
 const commonPath = path.join(__dirname, 'common');
 const src = path.join(commonPath, 'templates', fileName + '.pug');
-const dest = path.join(commonPath, 'public', 'templates', fileName + '.tmpl.js');
-
+const destDir = path.join(commonPath, 'public', 'templates');
+const dest = path.join(destDir, fileName + '.tmpl.js');
 const tmplStr = pug.compileFileClient(src, { name: 'todoListTemplate' });
+
+fs.mkdirSync(destDir);
 fs.writeFileSync(dest, tmplStr);
